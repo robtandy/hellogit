bb48b9cf54e598fc2c93133adc2f0b489a84109d
Alexander Glenn <aglenn@findawayworld.com>
2011-09-02 14:17:18 -0400

    Added a friendly greeting

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main (int argc, char **argv) {
-
-  printf ("Hello Git!");
+  char in[50];
+  printf ("Hello Git!\n");
+  printf("Enter Your Name: ");
+  scanf("%s", in);
+  printf("Hello, %s!\n", in);
+  return 0;
 }
