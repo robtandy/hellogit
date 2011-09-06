14a92c3aa250ec4d5212acd37cb00d18265ef883
RobTest Test <rob.tandy@gmail.com>
2011-09-06 08:25:23 -0400

    added some fancy talk

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@ int main (int argc, char **argv) {
 	/* This is a test git app */
 
 	char in[50];
-	printf ("Hello Git!");
+	printf ("Why, Hello there, Git!");
 	printf("\n");
 	printf("Enter Your Name: ");
 	scanf("%s", in);
