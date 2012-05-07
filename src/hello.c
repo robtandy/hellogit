c2aa00c43d70402029703a0e993ae28eeea33ee0
Ron McClellan <rmcclellan@findawayworld.com>
2012-05-07 15:04:42 -0400

    Changed a few typographical preferences

--- src/hello.c
+++ src/hello.c
@@ -5,9 +5,9 @@ int main (int argc, char **argv) {
 	/* This is a test git app */
 
 	char in[50];
-	printf ("Why, Hello there, Git!");
+	printf ("Why, Hello there, GitHub!");
 	printf("\n");
-	printf("Enter Your Name: ");
+	printf("Please Enter Your Full Name: ");
 	scanf("%s", in);
 	printf("Buon Giorno, %s!\n", in);
 	return (0);
